bb2a71ee37e8facb4f6eaec7c5b50cd6a998055e
Josefin Wall <josefin1010wall@gmail.com>
2026-08-02 10:35:07 +0200

    Make basic hero (without image).

---
+++ components/hero.tsx
@@ -0,0 +1,30 @@
+import Icon from "./icon";
+
+export default function Hero() {
+  return (
+    <section className="grid grid-cols-1 md:grid-cols-2 mx-fluid">
+      <div className="@container">
+        <h1 className="font-display text-fluid-6xl">Josefin Wall</h1>
+        <div className="flex flex-col items-start justify-end gap-8">
+          <a
+            href="https://github.com/josiefinis"
+            aria-label="github"
+            className="flex items-center gap-6 block-12 text-3xl hover:underline hover:underline-offset-12"
+          >
+            <Icon name="githubWhite" />
+            <span>josiefinis</span>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/josefin-wall-a513a4408/"
+            aria-label="linkedin"
+            className="flex items-center gap-4 block-12 text-3xl hover:underline hover:underline-offset-12"
+          >
+            <Icon name="linkedinWhite" />
+            <span>Josefin Wall</span>
+          </a>
+        </div>
+      </div>
+      <div></div>
+    </section>
+  );
+}
