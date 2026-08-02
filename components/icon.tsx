917033680054425f29ffd36742b6184475130c09
Josefin Wall <josefin1010wall@gmail.com>
2026-08-02 10:32:32 +0200

    Add icons.
    - Github logos
    - LinkedIn logos
    - Material symbols adjust icon

--- components/icon.tsx
+++ components/icon.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import code from "@/public/code.svg";
 import web from "@/public/web.svg";
+import adjust from "@/public/adjust.svg";
+import githubWhite from "@/public/github/GitHub_Invertocat_White.svg";
+import githubBlack from "@/public/github/GitHub_Invertocat_Black.svg";
+import linkedinWhite from "@/public/in-logo/InBug-White.png";
+import linkedinBlack from "@/public/in-logo/InBug-White.png";
+import linkedin from "@/public/in-logo/LI-In-Bug.png";
 
 interface IconProps {
   name: string;
@@ -10,6 +16,12 @@ interface IconProps {
 const icons = new Map([
   ["code", code],
   ["web", web],
+  ["adjust", adjust],
+  ["githubWhite", githubWhite],
+  ["githubBlack", githubBlack],
+  ["linkedinWhite", linkedinWhite],
+  ["linkedinBlack", linkedinBlack],
+  ["linkedin", linkedin],
 ]);
 
 export default function Icon({ name, className }: IconProps) {
