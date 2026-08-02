9adec1e65c7835bdecd78c76c441758033e1f869
Josefin Wall <josefin1010wall@gmail.com>
2026-08-02 10:29:07 +0200

    Simplify icon component.
    - Move wrapping anchor element out of icon component.

--- components/icon.tsx
+++ components/icon.tsx
@@ -4,8 +4,7 @@ import web from "@/public/web.svg";
 
 interface IconProps {
   name: string;
-  href: string;
-  className: string;
+  className?: string;
 }
 
 const icons = new Map([
@@ -13,18 +12,12 @@ const icons = new Map([
   ["web", web],
 ]);
 
-export default function Icon({ name, href, className }: IconProps) {
+export default function Icon({ name, className }: IconProps) {
   return (
-    <a
-      href={href}
-      target="_blank"
-      className={`p-2 rounded-ss-2xl rounded-ee-2xl hover:outline-2 ${className}`}
-    >
-      <Image
-        src={icons.get(name)}
-        alt={name}
-        className="block-10 inline-auto max-inline-10"
-      />
-    </a>
+    <Image
+      src={icons.get(name)}
+      alt={name}
+      className={`block-full inline-auto inline ${className}`}
+    />
   );
 }
